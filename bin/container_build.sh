6126d625115da268f67e1a4a5738523f81ad92e4
hibohiboo <swdice66@gmail.com>
2019-02-26 21:28:05 +0900

    developで動くことを確認

--- bin/container_build.sh
+++ bin/container_build.sh
@@ -2,5 +2,8 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
+parent_dir=$bin_dir/..
 
-cd $bin_dir/../docker && docker-compose build
+cp $parent_dir/package.json $parent_dir/docker/firebase/package.json
+cp $parent_dir/package-lock.json $parent_dir/docker/firebase/package-lock.json
+cd $parent_dir/docker && docker-compose build
